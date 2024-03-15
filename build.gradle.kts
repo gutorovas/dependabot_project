5d4560684cf9418d85bece95f9dcd57c2afb8a39
Gutorov <kruz@ecwid.com>
2024-03-15 14:09:37 +0400

    added libs

--- build.gradle.kts
+++ build.gradle.kts
@@ -1,3 +1,12 @@
+object Versions {
+    const val squareupApi = "23.1.0.20220823"
+    const val okhttp = "4.11.0"
+    const val squareupOkio = "3.4.0"
+    const val swaggerAnnotations = "1.5.8"
+    const val commonsCollectionsV3 = "3.2.2"
+    const val jackson = "2.15.2"
+}
+
 plugins {
     kotlin("jvm") version "1.9.22"
     application
@@ -12,6 +21,13 @@ repositories {
 
 dependencies {
     testImplementation(kotlin("test"))
+    implementation("com.squareup:square:${Versions.squareupApi}")
+    implementation("com.squareup.okhttp3:okhttp:${Versions.okhttp}")
+    api("com.squareup.okio:okio:${Versions.squareupOkio}")
+    implementation("io.swagger:swagger-annotations:${Versions.swaggerAnnotations}")
+    implementation("commons-collections:commons-collections:${Versions.commonsCollectionsV3}")
+    implementation("com.fasterxml.jackson.core:jackson-annotations:${Versions.jackson}")
+
 }
 
 tasks.test {
