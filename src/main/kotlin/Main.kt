5bd1a6355c9cf8a1758ca7f7b4e79b84affcbfe8
Gutorov <kruz@ecwid.com>
2024-03-15 13:56:54 +0400

    added some token

--- src/main/kotlin/Main.kt
+++ src/main/kotlin/Main.kt
@@ -1,6 +1,8 @@
 fun main(args: Array<String>) {
     println("Hello World!")
 
+    val token = "secret_w87c247824c9j85cyc8934t5v3497t5"
+
     // Try adding program arguments via Run/Debug configuration.
     // Learn more about running applications: https://www.jetbrains.com/help/idea/running-applications.html.
     println("Program arguments: ${args.joinToString()}")
