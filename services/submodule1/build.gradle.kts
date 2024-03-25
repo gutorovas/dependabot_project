c99c3fb1c6cf1d7d2cedb8c007f575b4cbffbc69
Gutorov <kruz@ecwid.com>
2024-03-25 17:36:08 +0400

    .

--- services/submodule1/build.gradle.kts
+++ services/submodule1/build.gradle.kts
@@ -32,8 +32,8 @@ dependencies {
 
     //!!    1.5.2
     implementation("wsdl4j:wsdl4j:1.6.3")
-    //!!     3.8.0
-    implementation("org.apache.zookeeper:zookeeper:3.8.0")
+    //!!     3.8.0 -> 3.8.4
+    implementation("org.apache.zookeeper:zookeeper:3.8.4")
 }
 
 tasks.test {
