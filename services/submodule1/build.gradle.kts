d690d4445d7cf7500fd75c6082f054a8e1554e8f
Gutorov <kruz@ecwid.com>
2024-03-25 17:38:10 +0400

    .

--- services/submodule1/build.gradle.kts
+++ services/submodule1/build.gradle.kts
@@ -31,9 +31,9 @@ dependencies {
     implementation("com.fasterxml.jackson.core:jackson-annotations:${Versions.jackson}")
 
     //!!    1.5.2
-    implementation("wsdl4j:wsdl4j:1.6.3")
+    implementation("wsdl4j:wsdl4j:1.5.2")
     //!!     3.8.0 -> 3.8.4
-    implementation("org.apache.zookeeper:zookeeper:3.8.4")
+    implementation("org.apache.zookeeper:zookeeper:3.8.0")
 }
 
 tasks.test {
